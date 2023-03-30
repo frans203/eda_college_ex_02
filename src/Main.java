c3cc51f0b3af903c3974ba6da8f6116f7c31aae1
Francisco Santana <franciscosantana203@gmail.com>
2023-03-30 09:09:59 -0300

    stack exercise

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        //Empty List
-        LinkedList linkedlist = new LinkedList();
-        //Verifying if List is empty
-        System.out.println(linkedlist.isEmpty());
-        //Adding elements to the front
-        linkedlist.addToFront(1);
-        linkedlist.addToFront(2);
-        linkedlist.addToFront(3);
-        //Get Size of the linkedList
-        System.out.println("size: " + linkedlist.getSize());
-        //Get element by position
-        System.out.println("Element at position 2: " + linkedlist.getElementByPosition(2));
-        //Get Position by Element
-        System.out.println("Position of element 3: " + linkedlist.getPositionByElement(3));
-        //Adding to tail of the LinkedList
-        linkedlist.addToTail(10);
-        linkedlist.addToTail(33);
-        //Modifying content of an element by position
-        linkedlist.modifyElement(2, 99);
-        //Insert Element on a certain position
-        linkedlist.insertElement(2, 49);
-        //Remove Element on a certain position
-        linkedlist.removeElement(4);
-        //Remove from Head
-        linkedlist.removeFromFront();
-        //Remove from tail
-        linkedlist.removeFromTail();
-        //Print List
-        linkedlist.printList();
+//        //Empty List
+//        LinkedList linkedlist = new LinkedList();
+//        //Verifying if List is empty
+//        System.out.println(linkedlist.isEmpty());
+//        //Adding elements to the front
+//        linkedlist.addToFront(1);
+//        linkedlist.addToFront(2);
+//        linkedlist.addToFront(3);
+//        //Get Size of the linkedList
+//        System.out.println("size: " + linkedlist.getSize());
+//        //Get element by position
+//        System.out.println("Element at position 2: " + linkedlist.getElementByPosition(2));
+//        //Get Position by Element
+//        System.out.println("Position of element 3: " + linkedlist.getPositionByElement(3));
+//        //Adding to tail of the LinkedList
+//        linkedlist.addToTail(10);
+//        linkedlist.addToTail(33);
+//        //Modifying content of an element by position
+//        linkedlist.modifyElement(2, 99);
+//        //Insert Element on a certain position
+//        linkedlist.insertElement(2, 49);
+//        //Remove Element on a certain position
+//        linkedlist.removeElement(4);
+//        //Remove from Head
+//        linkedlist.removeFromFront();
+//        //Remove from tail
+//        linkedlist.removeFromTail();
+//        //Print List
+//        linkedlist.printList();
+//
+//        //empty stack
+//        Stack stack = new Stack(10);
+//        //pushing element to stack
+//        stack.push(10);
+//        stack.push(9);
+//        stack.push(13);
+//        //is empty
+//        System.out.println("Is empty?: " + stack.isEmpty());
+//        //peek element from the top
+//        System.out.println(stack.top());
+//        //removing element from the top
+//        System.out.println("Removing from top: " + stack.pop());
+//        //size of the stack
+//        System.out.println("size: " + stack.size());
+//        //printing stack
+//        stack.printStack();
+
     }
 }
